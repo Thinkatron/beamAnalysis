87782b12d1d7e3230f877a968b9e2bc73507810f
Shawn McAnalley <amcanalley16@gmail.com>
2017-07-10 13:11:34 -0500

    did file parsing

--- electron/renderer.js
+++ electron/renderer.js
@@ -5,9 +5,34 @@
 const fs = require('fs');
 const readline = require('readline');
 
-var rl = readline.createInterface({ input: fs.createReadStream("data.txt"), output: process.stdout});
+var dataMatrix = new Array(721);
+for(var i = 0; i <= 720; i++){
+    dataMatrix[i] = new Array(1313).fill(0);
+}
+
+
+
+var rl = readline.createInterface({ input: fs.createReadStream("exData.TXT") });
 
 rl.on('line', function(line) {
-    document.write(line);
-})
+    var k = line.split("\t");
+    if (k.length >= 6) {
+        const x = k[1];
+        const y = k[2];
+        const luminosity = k[3];
+        dataMatrix[x][y] += (luminosity >= 200);
+    }
+});
+
+rl.on('close', (e) => {
+    console.log(dataMatrix);
+});
+
+
+
+function render() {
+    window.requestAnimationFrame(render);
+
+
+}
 
