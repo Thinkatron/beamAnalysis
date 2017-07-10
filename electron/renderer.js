56e763a49d25b6998a3add81868c52bd7181a51b
Garrett Gu <garrettgu777@gmail.com>
2017-07-10 11:27:36 -0500

    adds stuff

--- electron/renderer.js
+++ electron/renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+
+var rl = readline.createInterface({ input: fs.createReadStream("data.txt"), output: process.stdout});
+
+rl.on('line', function(line) {
+    document.write(line);
+})
+
